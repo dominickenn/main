4ec8436a84374ccd3fd9f02122a587f4e00802de
Dominic Kenn Lim <dominickenn@gmail.com>
2018-03-31 13:56:17 +0800

    Stylecheck fix

--- src/test/java/seedu/organizer/TestApp.java
+++ src/test/java/seedu/organizer/TestApp.java
@@ -104,8 +104,7 @@ public class TestApp extends MainApp {
         ui.start(primaryStage);
     }
 
-    public static void
-    main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 
