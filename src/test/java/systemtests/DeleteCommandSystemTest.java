d7832f3215f6cf0eeb6f924f1f546a75fb05dba8
dominickenn <dominickenn@gmail.com>
2018-04-02 14:22:09 +0800

    Revert "Comment out tests"
    This reverts commit 7f30caf0534033c830136577fb956bf99e22f9c1.

--- src/test/java/systemtests/DeleteCommandSystemTest.java
+++ src/test/java/systemtests/DeleteCommandSystemTest.java
@@ -37,34 +37,34 @@ public class DeleteCommandSystemTest extends OrganizerSystemTest {
         String command = "     " + DeleteCommand.COMMAND_WORD + "      " + INDEX_FIRST_TASK.getOneBased() + "       ";
         Task deletedTask = removePerson(expectedModel, INDEX_FIRST_TASK);
         String expectedResultMessage = String.format(MESSAGE_DELETE_PERSON_SUCCESS, deletedTask);
-        //assertCommandSuccess(command, expectedModel, expectedResultMessage);
+        assertCommandSuccess(command, expectedModel, expectedResultMessage);
 
         /* Case: delete the last task in the list -> deleted */
         Model modelBeforeDeletingLast = getModel();
         Index lastPersonIndex = getLastIndex(modelBeforeDeletingLast);
-        //assertCommandSuccess(lastPersonIndex);
+        assertCommandSuccess(lastPersonIndex);
 
         /* Case: undo deleting the last task in the list -> last task restored */
         command = UndoCommand.COMMAND_WORD;
         expectedResultMessage = UndoCommand.MESSAGE_SUCCESS;
-        //assertCommandSuccess(command, modelBeforeDeletingLast, expectedResultMessage);
+        assertCommandSuccess(command, modelBeforeDeletingLast, expectedResultMessage);
 
         /* Case: redo deleting the last task in the list -> last task deleted again */
         command = RedoCommand.COMMAND_WORD;
         removePerson(modelBeforeDeletingLast, lastPersonIndex);
         expectedResultMessage = RedoCommand.MESSAGE_SUCCESS;
-        //assertCommandSuccess(command, modelBeforeDeletingLast, expectedResultMessage);
+        assertCommandSuccess(command, modelBeforeDeletingLast, expectedResultMessage);
 
         /* Case: delete the middle task in the list -> deleted */
         Index middlePersonIndex = getMidIndex(getModel());
-        //assertCommandSuccess(middlePersonIndex);
+        assertCommandSuccess(middlePersonIndex);
 
         /* ------------------ Performing delete operation while a filtered list is being shown ---------------------- */
         /* Case: filtered task list, delete index within bounds of organizer and task list -> deleted */
         showTasksWithName(KEYWORD_MATCHING_REVISION);
         Index index = INDEX_FIRST_TASK;
-        //assertTrue(index.getZeroBased() < getModel().getFilteredTaskList().size());
-        //assertCommandSuccess(index);
+        assertTrue(index.getZeroBased() < getModel().getFilteredTaskList().size());
+        assertCommandSuccess(index);
 
         /* Case: filtered task list, delete index within bounds of organizer but out of bounds of task list
          * -> rejected
@@ -72,7 +72,7 @@ public class DeleteCommandSystemTest extends OrganizerSystemTest {
         showTasksWithName(KEYWORD_MATCHING_REVISION);
         int invalidIndex = getModel().getOrganizer().getTaskList().size();
         command = DeleteCommand.COMMAND_WORD + " " + invalidIndex;
-        //assertCommandFailure(command, MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        assertCommandFailure(command, MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
 
         /* --------------------- Performing delete operation while a task card is selected ------------------------ */
         /* Case: delete the selected task -> task list panel selects the task before the deleted task */
@@ -84,31 +84,31 @@ public class DeleteCommandSystemTest extends OrganizerSystemTest {
         command = DeleteCommand.COMMAND_WORD + " " + selectedIndex.getOneBased();
         deletedTask = removePerson(expectedModel, selectedIndex);
         expectedResultMessage = String.format(MESSAGE_DELETE_PERSON_SUCCESS, deletedTask);
-        //assertCommandSuccess(command, expectedModel, expectedResultMessage, expectedIndex);
+        assertCommandSuccess(command, expectedModel, expectedResultMessage, expectedIndex);
 
         /* --------------------------------- Performing invalid delete operation ------------------------------------ */
         /* Case: invalid index (0) -> rejected */
         command = DeleteCommand.COMMAND_WORD + " 0";
-        //assertCommandFailure(command, MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
+        assertCommandFailure(command, MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
 
         /* Case: invalid index (-1) -> rejected */
         command = DeleteCommand.COMMAND_WORD + " -1";
-        //assertCommandFailure(command, MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
+        assertCommandFailure(command, MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
 
         /* Case: invalid index (size + 1) -> rejected */
         Index outOfBoundsIndex = Index.fromOneBased(
                 getModel().getOrganizer().getTaskList().size() + 1);
         command = DeleteCommand.COMMAND_WORD + " " + outOfBoundsIndex.getOneBased();
-        //assertCommandFailure(command, MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        assertCommandFailure(command, MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
 
         /* Case: invalid arguments (alphabets) -> rejected */
-        //assertCommandFailure(DeleteCommand.COMMAND_WORD + " abc", MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
+        assertCommandFailure(DeleteCommand.COMMAND_WORD + " abc", MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
 
         /* Case: invalid arguments (extra argument) -> rejected */
-        //assertCommandFailure(DeleteCommand.COMMAND_WORD + " 1 abc", MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
+        assertCommandFailure(DeleteCommand.COMMAND_WORD + " 1 abc", MESSAGE_INVALID_DELETE_COMMAND_FORMAT);
 
         /* Case: mixed case command word -> rejected */
-        //assertCommandFailure("DelETE 1", MESSAGE_UNKNOWN_COMMAND);
+        assertCommandFailure("DelETE 1", MESSAGE_UNKNOWN_COMMAND);
     }
 
     /**
@@ -128,9 +128,9 @@ public class DeleteCommandSystemTest extends OrganizerSystemTest {
 
     /**
      * Deletes the task at {@code toDelete} by creating a default {@code DeleteCommand} using {@code toDelete} and
-     * performs the same verification as {@code //assertCommandSuccess(String, Model, String)}.
+     * performs the same verification as {@code assertCommandSuccess(String, Model, String)}.
      *
-     * @see DeleteCommandSystemTest#//assertCommandSuccess(String, Model, String)
+     * @see DeleteCommandSystemTest#assertCommandSuccess(String, Model, String)
      */
     private void assertCommandSuccess(Index toDelete) {
         Model expectedModel = getModel();
@@ -150,34 +150,34 @@ public class DeleteCommandSystemTest extends OrganizerSystemTest {
      * 5. Asserts that the status bar's sync status changes.<br>
      * 6. Asserts that the command box has the default style class.<br>
      * Verifications 1 to 3 are performed by
-     * {@code OrganizerSystemTest#//assertApplicationDisplaysExpected(String, String, Model)}.
+     * {@code OrganizerSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.
      *
-     * @see OrganizerSystemTest#//assertApplicationDisplaysExpected(String, String, Model)
+     * @see OrganizerSystemTest#assertApplicationDisplaysExpected(String, String, Model)
      */
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage) {
-        //assertCommandSuccess(command, expectedModel, expectedResultMessage, null);
+        assertCommandSuccess(command, expectedModel, expectedResultMessage, null);
     }
 
     /**
-     * Performs the same verification as {@code //assertCommandSuccess(String, Model, String)} except that the browser url
+     * Performs the same verification as {@code assertCommandSuccess(String, Model, String)} except that the browser url
      * and selected card are expected to update accordingly depending on the card at {@code expectedSelectedCardIndex}.
      *
-     * @see DeleteCommandSystemTest#//assertCommandSuccess(String, Model, String)
-     * @see OrganizerSystemTest#//assertSelectedCardChanged(Index)
+     * @see DeleteCommandSystemTest#assertCommandSuccess(String, Model, String)
+     * @see OrganizerSystemTest#assertSelectedCardChanged(Index)
      */
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage,
                                       Index expectedSelectedCardIndex) {
         executeCommand(command);
-        //assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
+        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
 
         if (expectedSelectedCardIndex != null) {
-            //assertSelectedCardChanged(expectedSelectedCardIndex);
+            assertSelectedCardChanged(expectedSelectedCardIndex);
         } else {
-            //assertSelectedCardUnchanged();
+            assertSelectedCardUnchanged();
         }
 
-        //assertCommandBoxShowsDefaultStyle();
-        //assertStatusBarChangedExceptSaveLocation();
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarChangedExceptSaveLocation();
     }
 
     /**
@@ -188,17 +188,17 @@ public class DeleteCommandSystemTest extends OrganizerSystemTest {
      * 4. Asserts that the browser url, selected card and status bar remain unchanged.<br>
      * 5. Asserts that the command box has the error style.<br>
      * Verifications 1 to 3 are performed by
-     * {@code OrganizerSystemTest#//assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * {@code OrganizerSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
      *
-     * @see OrganizerSystemTest#//assertApplicationDisplaysExpected(String, String, Model)
+     * @see OrganizerSystemTest#assertApplicationDisplaysExpected(String, String, Model)
      */
     private void assertCommandFailure(String command, String expectedResultMessage) {
         Model expectedModel = getModel();
 
         executeCommand(command);
-        //assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
-        //assertSelectedCardUnchanged();
-        //assertCommandBoxShowsErrorStyle();
-        //assertStatusBarUnchanged();
+        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsErrorStyle();
+        assertStatusBarUnchanged();
     }
 }
