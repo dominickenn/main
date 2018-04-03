16b78fc9f32c8e2c0461373732acc320b013fd55
dominickenn <dominickenn@gmail.com>
2018-04-03 16:07:16 +0800

    Add UserWithQuestionAnswer class

---
+++ src/main/java/seedu/organizer/model/user/UserWithQuestionAnswer.java
@@ -0,0 +1,64 @@
+package seedu.organizer.model.user;
+
+//@@author dominickenn
+
+import static seedu.organizer.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * A user class with a question-answer for password retrieval
+ */
+public class UserWithQuestionAnswer extends User {
+
+    public static final String MESSAGE_QUESTION_ANSWER_CONSTRAINTS =
+            "Questions and answers can take any values, and can be blank";
+
+    /*
+     * The first character must not be a whitespace, otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String QUESTION_VALIDATION_REGEX = ".*";
+    public static final String ANSWER_VALIDATION_REGEX = ".*";
+
+    public final String question;
+    public final String answer;
+
+    /**
+     * Constructs a {@code UserWithQuestionAnswer}.
+     *
+     * @param username A valid username.
+     * @param password A valid password.
+     */
+    public UserWithQuestionAnswer(String username, String password) {
+        super(username, password);
+        question = null;
+        answer = null;
+    }
+
+    /**
+     * Constructs a {@code UserWithQuestionAnswer}.
+     *
+     * @param username A valid username.
+     * @param password A valid password.
+     * @param question A valid question.
+     * @param answer A valid answer.
+     */
+    public UserWithQuestionAnswer(String username, String password, String question, String answer) {
+        super(username, password);
+        requireAllNonNull(question, answer);
+        this.question = question;
+        this.answer = answer;
+    }
+
+    /**
+     * Returns true if a given string is a valid question.
+     */
+    public static boolean isValidQuestion(String test) {
+        return test.matches(QUESTION_VALIDATION_REGEX);
+    }
+
+    /**
+     * Returns true if a given string is a valid answer.
+     */
+    public static boolean isValidAnswer(String test) {
+        return test.matches(ANSWER_VALIDATION_REGEX);
+    }
+}
